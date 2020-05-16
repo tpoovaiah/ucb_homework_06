4ece2832d1d20344a921ee9c5199a9e54143d7d1
tanyap <biddanda.tanya@gmail.com>
2020-05-16 08:56:46 -0700

    adds working ajax call

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
 $(document).ready(function(){
 
+    var appID = "18e8d96494d93797eebb6e49b5194ab5";
+    
+    var queryTerm = "";
+    
+    
+    var queryURLBase = "https://api.openweathermap.org/data/2.5/forecast?appID=" + appID;
+    
+
+
+    function runQuery (numForecast, queryURL) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .done(function(forecastData){
+            // console.log( "queryURL",queryURL);
+            // console.log("forecastData", forecastData);
+            // console.log("numForecast", numForecast)
+        })
+    }
+
+
+
+
     $(".btn").on("click", function(){
         event.preventDefault()
+
+        queryTerm = $("#cityName").val().toLowerCase().trim();
+
+        // console.log("queryTerm", queryTerm);
+
+        var newURL = queryURLBase + "&q=" + queryTerm
+        // console.log("newURL", newURL);
+
+        runQuery(5, newURL);
+
+        
+
+
     })
+
+
+    // var cityInput = //source the city name from the result of the ajax query
+
+
+    // var cities = []
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
 })
